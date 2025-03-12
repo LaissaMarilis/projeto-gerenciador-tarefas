beea67c154f6be897ade40ce4d7eec161b8e29c2
Laissa Marilis <laissa.marilis@outlook.com>
2025-03-12 10:22:32 -0300

    Initial commit

---
+++ src/test/java/com/tarefas/gerenciador/GerenciadorTarefasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarefas.gerenciador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorTarefasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
